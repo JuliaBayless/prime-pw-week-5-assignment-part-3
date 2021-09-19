f2c5b02448160fe5ffe232ced105f011e86d3643
Julia Bayless <julia.allistair@gmail.com>
2021-09-19 10:29:08 -0500

    working on addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+addToCollection( title , artist , yearPublished ) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+  return collection
+}
