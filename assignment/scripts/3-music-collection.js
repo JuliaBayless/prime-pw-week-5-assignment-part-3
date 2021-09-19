073351e5c8c7361d72760a29c23898ba187c0333
Julia Bayless <julia.allistair@gmail.com>
2021-09-19 13:38:26 -0500

    Created working showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,3 +19,11 @@ addToCollection('All Eyez on Me', 'Tupac Shakur', '1996');
 addToCollection('Songs of Faith and Devotion', 'Depeche Mode', '1993');
 addToCollection('Energía', 'J Balvin', '2016');
 addToCollection('A Day at the Races', 'Queen', '1976')
+
+function showCollection(arrayP) {
+  console.log('The number of albums logged is', arrayP.length);
+  for(let i = 0; i < arrayP.length; i++){
+    console.log(arrayP[i].title, 'by', arrayP[i].artist, 'published in', arrayP[i].yearPublished);
+  }
+};
+showCollection(collection);
